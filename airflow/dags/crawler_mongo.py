1bed4956487363cb3837a694cfd6d53de26c6566
José Adelmar <jose.adelmar@hotmail.com>
2023-06-25 23:11:11 -0300

    remove unused convert

--- airflow/dags/crawler_mongo.py
+++ airflow/dags/crawler_mongo.py
@@ -8,7 +8,7 @@ utility = Utility()
 s3 = S3()
 criterial = {"sexo": "Mulher", "idade": {"$gte": 20, "$lte": 40}}
 
-result_list = list(mongodb_client.get_data(criterial))
+result_list = mongodb_client.get_data(criterial)
 
 result_parsed = utility.parse_json(result_list)
 s3.write_file_in_bucket(f"mongodb/data.json", json.dumps(result_parsed))
